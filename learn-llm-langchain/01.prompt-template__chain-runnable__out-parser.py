7ffc063c83a2e9aecd43b9ee7e3640accc3d7d80
Duong Ngo <DuongNgo>
2024-12-26 10:42:31 +0700

    Load dotenv

--- learn-llm-langchain/01.prompt-template__chain-runnable__out-parser.py
+++ learn-llm-langchain/01.prompt-template__chain-runnable__out-parser.py
@@ -3,6 +3,9 @@ from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.messages import HumanMessage, SystemMessage
+from dotenv import load_dotenv
+
+load_dotenv()
 
 model = ChatOpenAI(model="gpt-4o-mini")
 
