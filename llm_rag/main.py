61839d75bd97d67a06e1a27ee8da5cc9f7ea80d2
Duong Ngo <DuongNgo>
2024-12-29 16:43:56 +0700

    Add HTML document loader and update main function to process website links

--- llm_rag/main.py
+++ llm_rag/main.py
@@ -1,10 +1,11 @@
 import sys
 import os
 import glob
+import requests
 
 from langchain_chroma import Chroma
 from langchain_openai import ChatOpenAI
-
+from bs4 import BeautifulSoup
 # Add the parent directory to sys.path
 current_dir = os.path.dirname(__file__)
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -18,7 +19,8 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.vectorstores import InMemoryVectorStore
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader, BSHTMLLoader
+from langchain_core.documents import Document
 load_dotenv()
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 # Document loader 
@@ -40,6 +42,21 @@ def get_pdf_files_path(folder_path):
     files = glob.glob(os.path.join(folder_path, '*.pdf'))
     return files
 
+def get_files_path(folder_path, extension):
+    files = glob.glob(os.path.join(folder_path, f'*.{extension}'))
+    return files
+
+def load_html_from_link(link):
+    response = requests.get(link)
+    response.raise_for_status()  # Raise an error for bad status codes
+    html_content = response.text
+    return html_content
+
+def create_documents_from_html(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    text = soup.get_text()
+    return [Document(page_content=text)]
+
 # Example usage
 documents = read_text_files(get_absolute_path(current_dir, './data_source/text'))
 text_splitter = RecursiveCharacterTextSplitter(
@@ -60,9 +77,25 @@ for pdf_file in pdf_files:
     pages = pdf_loader.load_and_split()
     pdf_documents.extend(pages)
 
+# Webite loader
+# read all links from file
+website_links = []
+website_file_links = get_files_path(get_absolute_path(current_dir, './data_source/website'), 'txt')
+for file_path in website_file_links:
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            website_links.append(line.strip())
+
+# Load HTML content from links
+html_documents = []
+for link in website_links:
+    html_content = load_html_from_link(link)
+    html_docs = create_documents_from_html(html_content)
+    html_documents.extend(html_docs)
+
 embedding = EmbeddingFactory.create_embedding(SENTENCE_TRANSFORMER_ALL_MPNET)
 vectorstore = InMemoryVectorStore.from_documents(
-    text_documents + pdf_documents,
+    text_documents + pdf_documents + html_documents,
     embedding=embedding,
 )
 
